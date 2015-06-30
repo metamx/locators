6126940e3fe2919b59200e52480d7f2e9d8052dc
Clint Wylie <cjwylie@gmail.com>
2015-06-30 10:28:40 -0700

    add comment about zookeeper fix for future archaeologists

--- src/zookeeper.ts
+++ src/zookeeper.ts
@@ -253,6 +253,9 @@ export function zookeeperLocatorFactory(parameters: ZookeeperLocatorParameters):
           disconnectedHandler = setTimeout(
             function () {
               client.close();
+              
+              // further delaying the emitt of 'expired' event seems to avoid
+              // intermittent errors thrown by node-zookeeper-client
               setTimeout(function () {
                 client.emit("expired");
               }, 1000);
